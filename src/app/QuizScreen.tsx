26a8f5cd8fbb4d091f6f00462db2174ce3c24425
Myatthiri <myatthirikhin1998@gmail.com>
2024-11-29 23:02:21 +0630

    feat: add icons and refactor UI

--- src/app/QuizScreen.tsx
+++ src/app/QuizScreen.tsx
@@ -1,5 +1,6 @@
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, Button, Pressable } from "react-native";
 import QuestionCard from "../components/QuesitionCard";
+import { FontAwesome6 } from "@expo/vector-icons";
 
 export default function QuizScreen() {
   return (
@@ -12,9 +13,18 @@ export default function QuizScreen() {
           <QuestionCard />
           <Text style={styles.time}>20 sec</Text>
         </View>
-        <View>
-          <Text>Footer</Text>
-        </View>
+        <Pressable onPress={()=>console.warn('Pressed')
+        } 
+        onLongPress={()=> console.warn('Long Press')}
+        style={styles.button}>
+          <Text style={styles.buttonText}>Next</Text>
+          <FontAwesome6
+          name='arrow-right-long'
+          size={16}
+          color="white"
+          style={styles.buttonIcon}
+          />
+        </Pressable>
       </View>
     </SafeAreaView>
   );
@@ -40,4 +50,21 @@ const styles = StyleSheet.create({
     color: "#005055",
     fontWeight: "bold",
   },
+  button: {
+    backgroundColor: "#005055",
+    padding: 20,
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '500',
+    fontSize: 16,
+    letterSpacing: 1.5
+  },
+  buttonIcon:{
+    position: 'absolute',
+    right: 10,
+  }
 });
